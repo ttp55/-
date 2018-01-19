c16af8bf85b29d47e5892ea7fdb927511814028d
WZG <1047697347@qq.com>
2018-01-19 14:51:28 +0800

    运动

--- 自学过程/js/startMove.js
+++ 自学过程/js/startMove.js
@@ -4,7 +4,16 @@ yundong:function startMove(obj,attr,iTarget)
 				clearInterval(obj.time);
 				obj.time=setInterval(function()
 					{
-						var iCur=parseInt(getStyle(obj, attr));//调用getStyle并传参数
+						var iCur=0;
+						if(attr=="opacity")
+						{
+							var iCur=parseInt(parseFloat(getStyle(obj, attr))*100);
+						}
+						else
+						{
+							var iCur=parseInt(getStyle(obj, attr));
+						}
+						//调用getStyle并传参数
 						var iSpeed=(iTarget-iCur)/8;
 						iSpeed=iSpeed>0?Math.ceil(iSpeed):Math.floor(iSpeed);
 						if(iCur==iTarget)
@@ -13,7 +22,15 @@ yundong:function startMove(obj,attr,iTarget)
 						}
 						else
 						{
-							obj.style[attr]=iCur+iSpeed+'px';
+							if(attr=="opacity")
+							{
+								obj.style.filter="alpha(opacity:'+(iCur+iSpeed)+')";
+								obj.style.opacity=(iCur+iSpeed)/100;
+							}
+							else
+							{
+								obj.style[attr]=iCur+iSpeed+'px';
+							}
 						}
 						
 					}
